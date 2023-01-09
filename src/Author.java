4bf32fc855f5bf64e85b1801a5bd15b9f8788d0b
Irina <sheldipower@gmail.com>
2023-01-09 23:21:15 +0500

    2

--- src/Author.java
+++ src/Author.java
@@ -1,15 +1,8 @@
 public class Author {
-    private String authorName;
-    private String authorSurname;
+    String authorName;
+    String authorSurname;
     public Author (String authorName, String authorSurname) {
         this.authorName = authorName;
         this.authorSurname=authorSurname;
-    }
- public String getAuthorName () {
-        return  this.authorName;
- }
-
-    public String getAuthorSurname() {
-        return this.authorSurname;
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,23 +1,13 @@
 import javax.security.sasl.AuthorizeCallback;
 
 public class Book {
-    private String nameBook;
-    private Author authorName;
-    private int theYearOfPublishing;
-
-    public String getNameBook () {
-        return this.nameBook;
-    }
-    public Author getAuthorName () {
-        return this.authorName;
-    }
-
-    public int getTheYearOfPublishing() {
-        return  this.theYearOfPublishing;
-    }
-
-    public void setTheYearOfPublishing() {
-        this.theYearOfPublishing = theYearOfPublishing;
-    }
+    String nameBook;
+    Author authorName;
+    int theYearOfPublishing;
+public  Book(String nameBook, Author authorName, int theYearOfPublishing) {
+    this.nameBook = nameBook;
+    this.authorName = authorName;
+    this.theYearOfPublishing = theYearOfPublishing;
+}
 }
 
